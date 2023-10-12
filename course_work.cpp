a224549b1fa3fbfd28745dfe1a2717250984ad51
Vitos <sopervitos2004@gmail.com>
2023-10-12 16:52:57 +0300

    ver.1

--- course_work.cpp
+++ course_work.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
 
+class Pizzeria;
+class Employee;
+class Client;
+class Order;
+class Pizza;
 
+class Pizzeria {
+public:
+
+private:
+	std::vector<Employee> workers;
+	std::stack<Order> current_orders;
+};
+
+class Employee {
+public:
+
+private:
+	std::string name;
+	
+};
+
+class Client {
+public:
+
+private:
+	std::string name;
+	std::string address;
+};
+
+class Pizza {
+public:
+	std::string getType() {
+		return pizza_type;
+	}
+	double getPrice() {
+		return price;
+	}
+private:
+	std::string pizza_type;
+	double price;
+};
+
+class Order{
+public:
+	double getOrderPrice() {
+		double sum = 0;
+		for (auto now : pizzas) {
+			sum += now.getPrice();
+		}
+		return sum;
+	}
+private:
+	std::vector<Pizza> pizzas;
+}
 
 
 
